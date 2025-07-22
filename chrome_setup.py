d3b10903608e0c5f21571d63bd17002d319af9e0
tharunbudidha <134671151+BudidhaTharun@users.noreply.github.com>
2025-07-22 10:34:51 +0530

    Update chrome_setup.py

--- chrome_setup.py
+++ chrome_setup.py
@@ -1,1 +1,15 @@
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+
+    return webdriver.Chrome(options=chrome_options)
